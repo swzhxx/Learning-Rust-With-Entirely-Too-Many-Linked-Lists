795c95dd8ca6102752a4fbb4366a4299422cf38e
swzhxx <549909896@qq.com>
2021-03-11 22:06:38 +0800

    feat(2-2): A bad Stack

---
+++ src/first.rs
@@ -0,0 +1,21 @@
+struct Node {
+    elem: i32,
+    next: List,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+pub struct List {
+    head: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+      List {
+        head:Link::Empty
+      }
+    }
+}
