c1586e61184d0b9a3914d06389c859560c4443b1
swzhxx <549909896@qq.com>
2021-03-17 20:12:54 +0800

    feat(5.3 Breaking): 5.3 Breaking

--- src/fourth.rs
+++ src/fourth.rs
@@ -31,6 +31,7 @@ impl<T> List<T> {
             tail: None,
         }
     }
+
     pub fn push_front(&mut self, elem: T) {
         let new_head = Node::new(elem);
         match self.head.take() {
@@ -44,5 +45,51 @@ impl<T> List<T> {
                 self.head = Some(new_head);
             }
         }
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.head.take().map(|old_head| {
+            match old_head.borrow_mut().next.take() {
+                Some(new_head) => {
+                    // -1 new
+                    // not emptying list
+                    new_head.borrow_mut().prev.take(); // -1 old
+                    self.head = Some(new_head); // +1 new
+                                                // total: -2 old, +0 new
+                }
+                None => {
+                    // emptying list
+                    self.tail.take(); // -1 old
+                                      // total: -2 old, (no new)
+                }
+            }
+            Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+        assert_eq!(list.pop_front(), None);
+
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), Some(2));
+
+        list.push_front(4);
+        list.push_front(5);
+
+        assert_eq!(list.pop_front(), Some(5));
+        assert_eq!(list.pop_front(), Some(4));
+
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), None);
     }
 }
