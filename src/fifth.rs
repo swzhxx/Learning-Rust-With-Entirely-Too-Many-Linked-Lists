b4a9a7b5d2d7218963ea452b15b0ac2f0614ba76
swzhxx <549909896@qq.com>
2021-03-18 21:58:52 +0800

    feat(5.5 未完): 5.5 未完

---
+++ src/fifth.rs
@@ -0,0 +1,43 @@
+use std::mem;
+
+pub struct List<'a, T> {
+    head: Link<T>,
+    tail: Option<&'a mut Node<T>>, // NEW!
+}
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<'a, T> List<'a, T> {
+    pub fn new() -> Self {
+        List {
+            head: None,
+            tail: None,
+        }
+    }
+
+    pub fn push(&'a mut self, elem: T) {
+        let new_tail = Box::new(Node {
+            elem: elem,
+            next: None,
+        });
+        // Put the box in the right place, and then grab a reference to its Node
+        let new_tail = match self.tail.take() {
+            Some(old_tail) => {
+                // If the old tail existed, update it to point to the new tail
+                old_tail.next = Some(new_tail);
+                old_tail.next.as_deref_mut()
+            }
+            None => {
+                // Otherwise, update the head to point to it
+                self.head = Some(new_tail);
+                self.head.as_deref_mut()
+            }
+        };
+
+        self.tail = new_tail;
+    }
+}
